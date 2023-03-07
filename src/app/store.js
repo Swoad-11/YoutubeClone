6b668bae7f2f1d882fd77d05dcc7079bb8766564
Swoad-11 <toufiq11swoad@gmail.com>
2023-03-08 01:07:21 +0600

    filter feature

--- src/app/store.js
+++ src/app/store.js
@@ -3,12 +3,14 @@ import videosReducer from '../features/videos/videosSlice';
 import relatedVideosReducer from '../features/relatedVideos/relatedVideosSlice';
 import videoReducer from '../features/video/videoSlice';
 import tagsReducer from '../features/tags/tagsSlice';
+import filterReducer from '../features/filter/filterSlice';
 
 export const store = configureStore({
   reducer: {
     videos: videosReducer,
     video: videoReducer,
     relatedVideos: relatedVideosReducer,
-    tags: tagsReducer
+    tags: tagsReducer,
+    filter: filterReducer
   },
 });
--- src/components/navbar/SearchForm.js
+++ src/components/navbar/SearchForm.js
@@ -1,19 +1,30 @@
-import React from 'react';
-import search from '../../assets/search.svg'
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import searchSVG from '../../assets/search.svg'
+import { searched } from '../../features/filter/filterSlice'
 
 const SearchForm = () => {
+    const dispatch = useDispatch();
+    const { search } = useSelector((state) => state.filter)
+    const { input, setInput } = useState(search);
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        dispatch(searched(input));
+    }
     return (
         <div>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <input
                     className="outline-none border-none mr-2"
                     type="search"
                     name="search"
                     placeholder="Search"
+                    value={input}
+                    onChange={(e) => setInput(e.target.value)}
                 />
                 <img
                     className="inline h-4 cursor-pointer"
-                    src={search}
+                    src={searchSVG}
                     alt="Search"
                 />
             </form>
